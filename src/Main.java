0262c347ebdb1dfd5c8f2fcff38c45df04fdfabd
Kasper-Bahr <kasperbahr@wp.pl>
2023-01-21 11:48:59 +0100

    Tablice zadanie 2

--- src/Main.java
+++ src/Main.java
@@ -9,7 +9,13 @@ public class Main {
             System.out.println("tab{" + i + "} = " + tab[i]);
         }
 
-        int[] tab
+        int[] tab1 = new int [10];
+        int licznik2 = 0;
+        for (int i = 10; i <= 100; i += 10){
+            tab1[licznik2++] = i;
+        }
+        for (int i =0; i < 10; i++)
+            System.out.println("tab {" + i + "} = " + tab1[i]);
 
 
     }
