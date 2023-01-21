0ac3562e686df4460154e490f433958c96078c85
Kasper-Bahr <kasperbahr@wp.pl>
2023-01-21 11:44:55 +0100

    Tablice zadanie 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] tab = new int[10];
+        int licznik = 0;
+        for (int i =101; i <=110; i++){
+            tab[licznik++] = i;
+        }
+        for (int i =0; i < 10; i++){
+            System.out.println("tab{" + i + "} = " + tab[i]);
+        }
+
+        int[] tab
+
+
     }
 }
